d1351d5d3b76fdb3945646992e79e215010bda0c
hadiyapranjalit23d1-cloud <hadiyapranjal.it23d1@scet.ac.in>
2026-06-18 19:00:24 +0530

    fix cors origin

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Allow frontend to communicate with the API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://document-signature-app-liart.vercel.app/"],
+    allow_origins=["http://localhost:5173","https://document-signature-app-liart.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
